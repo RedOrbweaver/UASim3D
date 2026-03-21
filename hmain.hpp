1ecfa3b10c4f02b32d1e6cf84ac31712ac642b86
user <RedOrbweaver>
2026-03-21 16:08:24 +0100

    cleanup

---
+++ hmain.hpp
@@ -0,0 +1,25 @@
+// #include <glad/glad.h>
+#include <GLES3/gl3.h>
+#include <GL/glew.h>
+#include <EGL/egl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <thread>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <cctype>
+#include "libraries/imgui-master/imgui.h"
+#include "libraries/imgui-master/imgui_impl_glfw.h"
+#include "libraries/imgui-master/imgui_impl_opengl3.h"
+#include "libraries/imgui-master/imgui_impl_opengl3_loader.h"
+
+#include "physics.hpp"
+#include "render.hpp"
